3370f8657915536d864fd3c17d1c04f45b60af7e
h7ml <h7ml@qq.com>
2022-08-15 11:00:32 +0800

    feat(action): build page

--- src/button/__tests__/button.spec.ts
+++ src/button/__tests__/button.spec.ts
@@ -3,18 +3,17 @@ import Button from "../button";
 import { shallowMount } from "@vue/test-utils";
 import { describe, expect, test } from "vitest";
 // 测试分组
-describe('Button', () => {
-    // mount
-    test("mount  @vue/test-utils", () => {
-        // @vue/test-utils
-        const wrapper = shallowMount(Button, {
-            slots: {
-                default: 'Button'
-            }
-        });
-
-        // 断言
-        expect(wrapper.text()).toBe("Button");
-
+describe("Button", () => {
+  // mount
+  test("mount  @vue/test-utils", () => {
+    // @vue/test-utils
+    const wrapper = shallowMount(Button, {
+      slots: {
+        default: "Button",
+      },
     });
-})
+
+    // 断言
+    expect(wrapper.text()).toBe("Button");
+  });
+});
--- src/button/index.ts
+++ src/button/index.ts
@@ -1,2 +1,2 @@
 import Button from "./Button";
-export default Button ;
+export default Button;
